0712b92478ad3ebbff993509ee6314f9984b832f
nmd974 <jha.payet@gmail.com>
2020-11-18 15:19:05 +0400

    ligne php include du header

--- src/index.php
+++ src/index.php
@@ -13,22 +13,8 @@
 </head>
 <body>
     <!--HEADER-->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">Ventes aux enchères</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#accueil">Liste des enchères</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#produits">Ajouter une enchère</a></li>
+    <?php include 'includes/header.php'; ?>
 
-                </ul>
-            </div>
-        </div>
-        </div>
-    </nav>
     <!--LISTE DES PRODUITS-->
 
     <!--AJOUT NOUVELLE ENCHERE-->
