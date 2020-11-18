c2bae75978b8280becec6aa87d739431d4c0008c
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-18 16:00:05 +0400

    changement cards

--- src/includes/listeEnchere.php
+++ src/includes/listeEnchere.php
@@ -1,29 +1,32 @@
-<div class="container-fluid">
-    <h2 class="text-center">ARTICLES</h2>
+<div class="container-fluid mt-5">
+    <h2 class="text-center mb-5">ARTICLES</h2>
     
-    <div class="card-deck">
+    <div class="card-deck col-11 mx-auto">
         <div class="card" style="width: 18rem;">
           <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Description</h5>
+            <h4 class="display-6">3 €</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Enchérir</a>
           </div>
         </div>
         <div class="card" style="width: 18rem;">
           <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Description</h5>
+            <h4 class="display-6">3 €</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Enchérir</a>
           </div>
         </div>
         <div class="card" style="width: 18rem;">
           <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Description</h5>
+            <h4 class="display-6">3 €</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Enchérir</a>
           </div>
         </div>
     </div>
