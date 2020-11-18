1eca6e728a15736e412e5a430f17419eeb5aabbb
nmd974 <jha.payet@gmail.com>
2020-11-18 15:42:25 +0400

    modification bug navbar fixed en sticky

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,13 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark sticky-top" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">Ventes aux enchères</a>
+            <a class="navbar-brand js-scroll-trigger text-white" href="#page-top">Ventes aux enchères</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                 data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#accueil">Liste des enchères</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#produits">Ajouter une enchère</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white" id="listeenchere" href="#accueil">Liste des enchères</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger text-white" href="#produits">Ajouter une enchère</a></li>
 
                 </ul>
             </div>
