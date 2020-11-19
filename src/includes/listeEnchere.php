2f268de01fe4031971e342934c3a0523f287bcca
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-19 09:06:47 +0400

    img cards

--- src/includes/listeEnchere.php
+++ src/includes/listeEnchere.php
@@ -3,7 +3,7 @@
     
     <div class="card-deck col-11 mx-auto">
         <div class="card" style="width: 18rem;">
-          <img src="" class="card-img-top" alt="...">
+          <img src="../ressources/img/moran-8cMPxOqkLE8-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Description</h5>
             <h4 class="display-6">3 €</h4>
@@ -15,7 +15,7 @@
           </div>
         </div>
         <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="../ressources/img/nikolay-tarashchenko-fti42Pzxcis-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Description</h5>
             <h4 class="display-6">3 €</h4>
@@ -27,7 +27,7 @@
           </div>
         </div>
         <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="../ressources/img/xps-1gLdTsX3_70-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Description</h5>
             <h4 class="display-6">3 €</h4>
