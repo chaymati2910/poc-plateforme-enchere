d8aae0b334fc3b419dad008a7cdb3be620d597a9
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-19 09:39:50 +0400

    titre cards bold

--- src/includes/listeEnchere.php
+++ src/includes/listeEnchere.php
@@ -5,20 +5,20 @@
         <div class="card mr-lg-5 ml-lg-0 ml-2" style="width: 18rem;">
           <img src="../ressources/img/moran-8cMPxOqkLE8-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Description</h5>
-            <h4 class="display-6">3 €</h4>
+            <h5 class="card-title font-weight-bold">Description</h5>
+            <h4 class="display-6 font-weight-bold">3 €</h4>
             <p class="card-text">Prix du clic : 50 cts/clic</p>
             <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
             <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto">Enchérir</a>
+              <a href="#" class="btn-lg btn-primary mx-auto">Enchérir</a>
             </div>
           </div>
         </div>
         <div class="card mr-5 ml-lg-0 ml-2" style="width: 18rem;">
           <img src="../ressources/img/nikolay-tarashchenko-fti42Pzxcis-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Description</h5>
-            <h4 class="display-6">3 €</h4>
+            <h5 class="card-title font-weight-bold">Description</h5>
+            <h4 class="display-6 font-weight-bold">3 €</h4>
             <p class="card-text">Prix du clic : 50 cts/clic</p>
             <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
             <div class="d-flex justify-content-center">
@@ -29,8 +29,8 @@
         <div class="card ml-lg-0 ml-2" style="width: 18rem;">
           <img src="../ressources/img/xps-1gLdTsX3_70-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Description</h5>
-            <h4 class="display-6">3 €</h4>
+            <h5 class="card-title font-weight-bold">Description</h5>
+            <h4 class="display-6 font-weight-bold">3 €</h4>
             <p class="card-text">Prix du clic : 50 cts/clic</p>
             <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
             <div class="d-flex justify-content-center">
