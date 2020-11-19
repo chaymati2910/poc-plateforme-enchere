4b5e6fc91fddaaef00276360403827a4dc65761d
Sakil GOULAMALY <sakil113@gmail.com>
2020-11-19 08:52:55 +0400

    add img

--- src/includes/listeEnchere.php
+++ src/includes/listeEnchere.php
@@ -3,7 +3,7 @@
     
     <div class="card-deck col-11 mx-auto">
         <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Description</h5>
             <h4 class="display-6">3 €</h4>
