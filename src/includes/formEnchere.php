d3a2c0d939f1d6b725684b9c052f0aef802a5c60
nmd974 <jha.payet@gmail.com>
2020-11-20 11:01:35 +0400

    fixed bug responsive iphone5 taille en pourcentage des input et label

--- src/includes/formEnchere.php
+++ src/includes/formEnchere.php
@@ -4,7 +4,7 @@
     <form class="container-fluid w-100 d-flex justify-content-center align-items-center flex-column">
         <div class="d-flex justify-content-center align-items-center mb-3 items bg-light">
             <label class="labelForm" for="#description">Description :</label>
-            <textarea class="form-control" id="description" aria-label="With textarea"></textarea>
+            <textarea class="form-control" id="description" maxlength="24" placeholder="24 caractères maximum" aria-label="With textarea"></textarea>
         </div>
         <div class="d-flex justify-content-center align-items-center">
             <label class="fileUpload d-flex justify-content-center align-items-center bg-light">
@@ -19,19 +19,19 @@
         </div>  
         <div class="d-flex justify-content-center align-items-center mb-3 items bg-light">
             <label class="labelForm" for="#duree">Durée (h):</label>
-            <input type="number" class="form-control" aria-label="duree" id="duree" required aria-describedby="basic-addon1">
+            <input type="number" class="form-control" aria-label="duree" placeholder="exemple: 48 h" id="duree" required aria-describedby="basic-addon1">
         </div>
         <div class="d-flex justify-content-center align-items-center mb-3 items bg-light">
             <label class="labelForm" for="#prix_clic">Prix du clic (cts):</label>
-            <input type="number" class="form-control" aria-label="prix_clic" id="prix_clic" required aria-describedby="basic-addon1" max="0.99" min="0" step="0.05">
+            <input type="number" class="form-control" placeholder="exemple: 30 cts" aria-label="prix_clic" id="prix_clic" required aria-describedby="basic-addon1" max="1" min="0" step="1">
         </div>
         <div class="d-flex justify-content-center align-items-center mb-3 items bg-light">
             <label class="labelForm" for="#augmentation_prix">Augmentation du prix (cts):</label>
-            <input type="number" class="form-control" aria-label="augmentation_prix" required id="augmentation_prix" aria-describedby="basic-addon1" max="0.99" min="0.00" step="0.05">
+            <input type="number" class="form-control" aria-label="augmentation_prix" placeholder="exemple: 2 cts" required id="augmentation_prix" aria-describedby="basic-addon1" max="0.99" min="0.00" step="0.05">
         </div>
         <div class="d-flex justify-content-center align-items-center mb-3 items bg-light">
             <label class="labelForm" for="#augmentation_duree">Augmentation durée (s):</label>
-            <input type="number" class="form-control" aria-label="augmentation_duree" required id="augmentation_duree" aria-describedby="basic-addon1">
+            <input type="number" class="form-control" aria-label="augmentation_duree" placeholder="exemple: 30 s" required id="augmentation_duree" aria-describedby="basic-addon1">
         </div>
         <div class="d-flex justify-content-center align-items-center">
         <button type="submit" class="btn btn-warning text-uppercase text-white font-weight-bold btnAjoutEnchere mb-5" style="width:220px;">Ajouter l'enchère</button>
