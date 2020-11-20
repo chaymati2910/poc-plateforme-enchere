56f260c0bdbbdb4b28c302767e5d1b396fc2018d
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-20 09:38:48 +0400

    normalisation liste produits

--- src/includes/listeEnchere.php
+++ src/includes/listeEnchere.php
@@ -1,85 +1,86 @@
-<div class="container-fluid mt-5">
-    <h2 class="text-center mb-5">ARTICLES</h2>
+<div id="articles" class="container-fluid mt-5">
+    <h2 class="text-center mb-5 font-weight-bold">ARTICLES</h2>
     
     <div class="card-deck col-lg-10 mx-auto">
-        <div class="card mr-lg-5 ml-lg-0 ml-2" style="width: 18rem;">
-        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">15s</div>
+        <div class="card mr-lg-5 ml-lg-0 ml-2" style="width: 14rem;">
+        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">
+        00 : 00 : 00</div>
           <img src="../ressources/img/moran-8cMPxOqkLE8-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title font-weight-bold">Description</h5>
+            <h5 class="card-title font-weight-bold">Macbook avec logo dessus</h5>
             <h4 class="display-6 font-weight-bold">3 €</h4>
-            <p class="card-text">Prix du clic : 50 cts/clic</p>
-            <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
-            <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto w-50">Enchérir</a>
+            <p class="card-text m-0">Prix du clic : 50 cts/clic</p>
+            <p class="card-text mb-4">Prix de l'enchère : 1 cts/clic</p>
+            <div class="text-center">
+              <button class="btn btn-primary p-0">Enchérir</button>
             </div>
           </div>
         </div>
-        <div class="card mr-5 ml-lg-0 ml-2" style="width: 18rem;">
-        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">15s</div>
+        <div class="card mr-5 ml-lg-0 ml-2" style="width: 14rem;">
+        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">00 : 00 : 00</div>
           <img src="../ressources/img/nikolay-tarashchenko-fti42Pzxcis-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title font-weight-bold">Description</h5>
             <h4 class="display-6 font-weight-bold">3 €</h4>
-            <p class="card-text">Prix du clic : 50 cts/clic</p>
-            <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
-            <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto w-50">Enchérir</a>
+            <p class="card-text m-0">Prix du clic : 50 cts/clic</p>
+            <p class="card-text mb-4">Prix de l'enchère : 1 cts/clic</p>
+            <div class="text-center">
+              <button class="btn btn-primary p-0">Enchérir</button>
             </div>
           </div>
         </div>
-        <div class="card ml-lg-0 ml-2" style="width: 18rem;">
-        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">15s</div>
+        <div class="card ml-lg-0 ml-2" style="width: 14rem;">
+        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">00 : 00 : 00</div>
           <img src="../ressources/img/xps-1gLdTsX3_70-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title font-weight-bold">Description</h5>
             <h4 class="display-6 font-weight-bold">3 €</h4>
-            <p class="card-text">Prix du clic : 50 cts/clic</p>
-            <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
-            <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto w-50">Enchérir</a>
+            <p class="card-text m-0">Prix du clic : 50 cts/clic</p>
+            <p class="card-text mb-4">Prix de l'enchère : 1 cts/clic</p>
+            <div class="text-center">
+              <button class="btn btn-primary p-0">Enchérir</button>
             </div>
           </div>
         </div>
     </div>
 
     <div class="card-deck col-lg-10 mx-auto mt-lg-5 pb-lg-5">
-        <div class="card mr-lg-5 ml-lg-0 ml-2" style="width: 18rem;">
-        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">15s</div>
+        <div class="card mr-lg-5 ml-lg-0 ml-2" style="width: 14rem;">
+        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">00 : 00 : 00</div>
           <img src="../ressources/img/moran-8cMPxOqkLE8-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title font-weight-bold">Description</h5>
             <h4 class="display-6 font-weight-bold">3 €</h4>
-            <p class="card-text">Prix du clic : 50 cts/clic</p>
-            <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
-            <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto w-50">Enchérir</a>
+            <p class="card-text m-0">Prix du clic : 50 cts/clic</p>
+            <p class="card-text mb-4">Prix de l'enchère : 1 cts/clic</p>
+            <div class="text-center">
+              <button class="btn btn-primary p-0">Enchérir</button>
             </div>
           </div>
         </div>
-        <div class="card mr-5 ml-lg-0 ml-2" style="width: 18rem;">
-        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">15s</div>
+        <div class="card mr-5 ml-lg-0 ml-2" style="width: 14rem;">
+        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">00 : 00 : 00</div>
           <img src="../ressources/img/nikolay-tarashchenko-fti42Pzxcis-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title font-weight-bold">Description</h5>
             <h4 class="display-6 font-weight-bold">3 €</h4>
-            <p class="card-text">Prix du clic : 50 cts/clic</p>
-            <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
-            <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto w-50">Enchérir</a>
+            <p class="card-text m-0">Prix du clic : 50 cts/clic</p>
+            <p class="card-text mb-4">Prix de l'enchère : 1 cts/clic</p>
+            <div class="text-center">
+              <button class="btn btn-primary p-0">Enchérir</button>
             </div>
           </div>
         </div>
-        <div class="card ml-lg-0 ml-2" style="width: 18rem;">
-        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">15s</div>
+        <div class="card ml-lg-0 ml-2" style="width: 14rem;">
+        <div class="duree d-flex position-absolute w-50 justify-content-center align-items-center font-weight-bold">00 : 00 : 00</div>
           <img src="../ressources/img/xps-1gLdTsX3_70-unsplash.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title font-weight-bold">Description</h5>
             <h4 class="display-6 font-weight-bold">3 €</h4>
-            <p class="card-text">Prix du clic : 50 cts/clic</p>
-            <p class="card-text">Prix de l'enchère : 1 cts/clic</p>
-            <div class="d-flex justify-content-center">
-              <a href="#" class="btn btn-primary mx-auto w-50">Enchérir</a>
+            <p class="card-text m-0">Prix du clic : 50 cts/clic</p>
+            <p class="card-text mb-4">Prix de l'enchère : 1 cts/clic</p>
+            <div class="text-center">
+              <button class="btn btn-primary p-0">Enchérir</button>
             </div>
           </div>
         </div>
