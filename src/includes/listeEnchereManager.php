9a5c0cb0cac7d802d07aaf8a74ad9f4e1b947a7c
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-25 12:57:09 +0400

    link buttons

--- src/includes/listeEnchereManager.php
+++ src/includes/listeEnchereManager.php
@@ -64,11 +64,13 @@ if(isset($_POST['submit_desactiver'])){
                             <!-- <button class="btn btn-primary p-0 mb-2 mt-0" name="submit_activer">Activer</button>
                             <button class="btn btn-primary p-0 mt-0" name="submit_desactiver">Desactiver</button> -->
                             <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                                <label class="btn btn-secondary btn-manager active">
-                                    <input type="radio" name="options" id="option1" checked> Activer
+                                <label class="btn btn-secondary btn-manager active" name="">
+                                    <button id="option1" class="bg-transparent border-0 text-white font-weight-bold" name="submit_activer">Activer</button>
+                                    <!-- <input type="radio" name="submit_activer" id="option1" checked> Activer -->
                                 </label>
-                                <label class="btn btn-secondary btn-manager">
-                                    <input type="radio" name="options" id="option2"> Desactiver
+                                <label class="btn btn-secondary btn-manager" name="">
+                                    <button id="option2" class="bg-transparent border-0 text-white font-weight-bold" name="submit_desactiver">Desactiver</button>
+                                    <!-- <input type="radio" name="submit_desactiver" id="option2"> Desactiver -->
                                 </label>
                             </div>
                         </form>
