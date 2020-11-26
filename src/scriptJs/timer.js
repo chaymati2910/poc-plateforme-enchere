72ff2ba797ed6978621121adc2d0d073d85df00c
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-26 08:31:23 +0400

    Delete timer.js

--- src/scriptJs/timer.js
+++
@@ -1,8 +0,0 @@
-function myTimer(fin, actuel) {
-    var v_fin = fin;
-    var v_actuel = actuel; 
-    var myVar = setInterval(timer(v_fin, v_actuel), 1000);
-    function timer(v_fin, v_actuel){
-        return v_fin - v_actuel;
-    }
-}
